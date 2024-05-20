958b46051ea0990adc68f3b12ac1bce8c15b2802
JacksonBamber <90766057+JacksonBamber@users.noreply.github.com>
2024-05-20 21:07:59 +1000

    State Machine start
    start on state machine, this is the barebones class with no logic in there yet.

--- scripts/drone_republisher.py
+++ scripts/drone_republisher.py
@@ -4,6 +4,7 @@ import math
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import Imu
 from std_msgs.msg import Empty
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 import time
 
@@ -19,43 +20,42 @@ class droneRepublisher:
     def __init__(self):
         self.takeoff = rospy.Publisher('/tello/takeoff', Empty, queue_size=1)
         self.landing = rospy.Publisher('/tello/land', Empty, queue_size=1)
-        self.images = rospy.Publisher('/cam0/image_raw', Image, queue_size=1)
-        self.imu_pub = rospy.Publisher('/imu0', Imu, queue_size=1)
         self.cmd_pub= rospy.Publisher('/tello/cmd_vel', Twist, queue_size=1)
-        self.image = Image()
-        self.imu = Imu()
+        self.odom = Odometry()
 
-        # while self.takeoff.get_num_connections() < 1:
-        #    pass
-        # self.takeoff.publish(Empty())
-        # rospy.on_shutdown(self.land)
-
-
-    def image_republish(self, tello_image):
-        self.image = tello_image
-        image_time = time.time()
-        image_seconds = math.floor(time.time())
-        image_nanoseconds = round((image_time - image_seconds)*10**9)
-        self.image.header.stamp.secs = image_seconds
-        self.image.header.stamp.nsecs = image_nanoseconds
-        self.images.publish(self.image)
-
-
-        #Testing purposes
-        self.cmd_vel_publisher()
+        odom_sub = rospy.Subscriber('/tello/odom', Odometry,
+                lambda msg: drone.odom_subscriber(msg), queue_size=1)
         
 
+        while self.takeoff.get_num_connections() < 1:
+           pass
+        self.takeoff.publish(Empty())
 
-    def imu_republish(self, tello_imu):
-        self.imu = tello_imu
-        #Inserting timestamp into image and imu
-        pub_time = time.time()
-        time_seconds = math.floor(time.time())
-        time_nanoseconds = round((pub_time - time_seconds)*10**9)
-        self.imu.header.stamp.secs = time_seconds
-        self.imu.header.stamp.nsecs = time_nanoseconds
-        #publishing data
-        self.imu_pub.publish(self.imu)
+        rospy.on_shutdown(self.land)
+
+    def get_pose(self):
+        x = self.odom.pose.pose.position.x
+        y = self.odom.pose.pose.position.y
+        z = self.odom.pose.pose.position.z
+        return x, y, z
+
+    def get_twist(self):
+        print(self.odom.pose.pose.orientation)
+
+    def odom_subscriber(self, odom_data):
+        self.odom = odom_data
+
+    def trans_speeds(self, x_dot, y_dot, z_dot, x_ang, y_ang, z_ang):
+        command = Twist()
+        command.linear.x = x_dot
+        command.linear.y = y_dot
+        command.linear.z = z_dot
+        command.angular.x = x_ang
+        command.angular.y = y_ang
+        command.angular.z = z_ang
+        self.cmd_pub.publish(command)
+
+
     
     def cmd_vel_publisher(self):
         command = Twist()
@@ -78,10 +78,10 @@ if __name__ == '__main__':
 
     drone = droneRepublisher()
     
-    image_sub = rospy.Subscriber('/tello/camera/image_raw', Image,
-                lambda msg: drone.image_republish(msg), queue_size=1)
-    
-    imu_sub = rospy.Subscriber('/tello/imu', Imu,
-                lambda msg: drone.imu_republish(msg), queue_size=1)
     rospy.loginfo("Node has been Initialised")
-    rospy.spin()
+
+    while not rospy.is_shutdown():
+        print("Pose")
+        drone.get_pose()
+        print("twist")
+        drone.get_twist()
