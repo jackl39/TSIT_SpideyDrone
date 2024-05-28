57039dc4a2c2f162709d18941d4c651fefb2d894
amat7180 <101961565+amat7180@users.noreply.github.com>
2024-05-28 14:20:54 +1000

    removed pygames

--- scripts/autonomous_turtlebot.py
+++ scripts/autonomous_turtlebot.py
@@ -1,21 +1,11 @@
 #!/usr/bin/env python3
 import rospy
-import pygame
 from City import City
-
-def initialize_pygame():
-    pygame.init()
-    window_size = (1024, 1024)
-    window = pygame.display.set_mode(window_size)
-    pygame.display.set_caption("City Visualization")
-    return window
 
 if __name__ == '__main__':
     rospy.init_node('Localiser', anonymous=True)
 
     try:
-        #window = initialize_pygame()
         city = City()
-        # city.run()
     except rospy.ROSInterruptException:
         pass
